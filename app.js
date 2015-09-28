46346d733424f76969db8bcf749b4494f0df0420
Jared Bates <computerwizjared@hotmail.com>
2015-09-28 14:50:50 -0500

    Working on DB stuff

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('sqlite:database.db');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,4 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Help Desk'});
 });
 
-
-
 module.exports = router;
