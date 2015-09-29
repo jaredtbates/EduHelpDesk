43e4c07293600211894cc99ef41452acac006644
Jared Bates <computerwizjared@hotmail.com>
2015-09-29 14:41:53 -0500

    DB tweaks... work on config too

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var bodyParser = require('body-parser');
 
 var db = require('./lib/db');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = {
+    index: require('./routes/index'),
+    users: require('./routes/users')
+};
 
 var app = express();
 
@@ -24,8 +26,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes.index);
+app.use('/users', routes.users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
