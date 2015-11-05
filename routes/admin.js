a36ee7521052d51744f85bc2acb99d1c477c9bd2
Jared Bates <me@jaredbates.net>
2015-11-04 20:46:26 -0600

    Auth works except for the horrible redirect issue :(

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var db = require('../lib/db');
 
 router.get('/', function (req, res) {
+    if (!req.isAuthenticated()) {
+        res.redirect('/auth/google?next=admin');
+        return;
+    }
+
     db.Request.findAll().then(function(requests) {
         res.render('admin/index', {
             title: 'Help Desk | Admin',
--- routes/auth/google.js
+++ routes/auth/google.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-router.get('/', passport.authenticate('google', {
-    scope: 'profile'
-}));
+router.get('/', passport.authenticate('google', { scope: 'profile' }));
 
-router.get('/callback', passport.authenticate('google', {
-    successRedirect: '/'
-}));
+router.get('/callback', passport.authenticate('google', { successRedirect: '/' }));
 
 module.exports = router;
