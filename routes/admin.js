3c8ca3ba9f797e8ecf1d31ea5cb4bc988f057143
Jared Bates <computerwizjared@hotmail.com>
2015-12-23 10:20:23 -0500

    Working on no auth for testing and stuff

--- routes/admin.js
+++ routes/admin.js
@@ -5,12 +5,12 @@ var db = require('../lib/db');
 var appConfig = require('../config/app.json');
 
 router.get('/', function (req, res) {
-    if (!req.isAuthenticated()) {
+    if (!req.isAuthenticated() && !(appConfig.authProvider == 'none')) {
         res.redirect('/auth/' + appConfig.authProvider + '?next=admin');
         return;
     }
 
-    if (appConfig.admins.indexOf(req.user.emails[0].value) == -1) {
+    if (!(appConfig.authProvider == 'none') && appConfig.admins.indexOf(req.user.emails[0].value) == -1) {
         res.redirect('/');
         return;
     }
